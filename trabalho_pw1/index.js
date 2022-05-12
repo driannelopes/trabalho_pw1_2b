c2cc5ab3792ad4df71763f0a812ea24983aa9969
Adrianne <102295473+driannelopes@users.noreply.github.com>
2022-05-12 15:51:14 -0300

    Add files via upload

---
+++ trabalho_pw1/index.js
@@ -0,0 +1,6 @@
+function soma() {
+    var valor_base= document.getElementById ("valor_base")
+    var transporte= document.getElementById ("transporte")
+    var soma= valor_base+transporte
+    
+}
